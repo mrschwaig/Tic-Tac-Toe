072f4e59fa024798c1ec9084c6e55c3f7858aa46
Christoph Schwaighofer <christoph@hwaildesign.com>
2024-09-20 13:54:36 +0200

    Erster Commit

--- 20.09/main.cpp
+++ 20.09/main.cpp
@@ -7,9 +7,18 @@ using namespace std;
 string Passwort;
 string Name;
 
+string User1;
+string User2;
+
+int Zeile;
+int Spalte;
+
+int Zug;
+int win = 0;
+
 int iLade;
 
-char feldID[3][3] = { {'1', '2' , '3'}, {'1', '2' , '3'}, {'1', '2', '3'} };
+char feldID[3][3] = { {'0', '1' , '2'}, {'0', '1' , '2'}, {'0', '1', '2'} };
 
 int welcome(string Title) {
 	cout << Title << endl << "---------------------" << endl << "Willkommen " << Name;
@@ -22,6 +31,20 @@ int feld() {
 	cout << "| " << feldID[1][0] << " | " << feldID[1][1] << " | " << feldID[1][2] << " |" << endl;
 	cout << "|---|---|---|" << endl;
 	cout << "| " << feldID[2][0] << " | " << feldID[2][1] << " | " << feldID[2][2] << " |" << endl;
+
+	return 1;
+}
+
+int usernames() {
+	cout << endl << "Spielername 1: ";
+	cin >> User1;
+
+	cout << endl << "Spielername 2: ";
+	cin >> User1;
+
+	cout << endl << endl << "Los gehts!";
+
+	Sleep(1500);
 
 	return 1;
 }
@@ -69,7 +92,34 @@ int main(void) {
 
 	system("clear");
 
+	cout << "Dies ist das Spielfeld: " << endl;
+	Sleep(1000);
 	feld();
+
+	cout << endl << endl << "Lass und losstarten!";
+	system("PAUSE");
+
+	system("clear");
+	usernames();
+	system("clear");
+
+	Zug = 0;
+
+	do {
+		feld();
+
+		cout << endl << endl << "Spieler " << Zug % 2 << " setzt: " << endl << "Zeile: ";
+		cin >> Zeile;
+		cout << "Spalte: ";
+		cin >> Spalte;
+
+		char feldID[Zeile][Spalte] = { 'X' };
+
+		system("clear");
+		Zug++;
+	} while (win == 0);
+
+	
 
 	return 1;
 }
